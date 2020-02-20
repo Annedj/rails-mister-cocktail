2cfdb71492cc79037972cc5eb98f3f31ec3de9e4
Anne de Joly <anne.dejoly@edhec.com>
2020-02-20 18:12:12 +0000

    cocktails list and doses features

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+puts "Creating the ingredients..."
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+user = JSON.parse(user_serialized)
+
+ingredients = user['drinks']
+
+ingredients.each do |ingredient|
+  Ingredient.create!(name: ingredient['strIngredient1'])
+end
+
+puts "Ingredients created!"
