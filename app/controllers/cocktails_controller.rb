a029b64b05c99164d522c0f8864837aa5d4b0c16
Anne de Joly <anne.dejoly@edhec.com>
2020-02-21 15:42:59 +0000

    added cloudinary upload feature

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -29,6 +29,6 @@ class CocktailsController < ApplicationController
   end
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :image_url)
+    params.require(:cocktail).permit(:name, :picture)
   end
 end
